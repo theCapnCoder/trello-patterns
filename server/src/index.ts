091198a439af6482a8d686a3f6b148cfabb066f8
PavelM <myagkiypavel@gmail.com>
2024-07-29 13:07:57 +0300

    feat: add proxy

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,14 +1,18 @@
-import { createServer } from "http";
-import { Server, Socket } from "socket.io";
+import express from 'express';
+import { createServer } from 'http';
+import { Server, Socket } from 'socket.io';
+import { createProxyMiddleware } from 'http-proxy-middleware';
 
 import { lists } from "./assets/mock-data";
 import { Database } from "./data/database";
 import { CardHandler, ListHandler } from "./handlers/handlers";
 import { ReorderService } from "./services/reorder.service";
 
-const PORT = 3005;
+const PORT = process.env.PORT || 3000;
+const CLIENT_PORT = process.env.CLIENT_PORT || 5173;
 
-const httpServer = createServer();
+const app = express();
+const httpServer = createServer(app);
 const io = new Server(httpServer, {
   cors: {
     origin: "*",
@@ -22,6 +26,11 @@ const reorderService = new ReorderService();
 if (process.env.NODE_ENV !== "production") {
   db.setData(lists);
 }
+
+app.use('/', createProxyMiddleware({
+  target: `http://localhost:${CLIENT_PORT}`,
+  changeOrigin: true,
+}));
 
 const onConnection = (socket: Socket): void => {
   new ListHandler(io, db, reorderService).handleConnection(socket);
